dc6b2342c00bebedde0d2d8bfc8b02496b75413e
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2021-05-06 20:18:44 +0200

    Example of serialization of multiple attempts to execute applications.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ CXXFLAGS = -g -std=gnu++11 -O1 -Wall
 LDFLAGS = -lrt -lpthread
 #LDLIBS = -lm
 
-SOURCES = change_me.c mzapo_phys.c mzapo_parlcd.c
+SOURCES = change_me.c mzapo_phys.c mzapo_parlcd.c serialize_lock.c
 #SOURCES += font_prop14x16.c font_rom8x16.c
 TARGET_EXE = change_me
 #TARGET_IP ?= 192.168.202.127
--- change_me.c
+++ change_me.c
@@ -24,14 +24,32 @@
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
 #include "mzapo_regs.h"
+#include "serialize_lock.h"
 
 int main(int argc, char *argv[])
 {
+
+  /* Serialize execution of applications */
+
+  /* Try to acquire lock the first */
+  if (serialize_lock(1) <= 0) {
+    printf("System is occupied\n");
+
+    if (1) {
+      printf("Waitting\n");
+      /* Wait till application holding lock releases it or exits */
+      serialize_lock(0);
+    }
+  }
+
   printf("Hello world\n");
 
   sleep(4);
 
   printf("Goodbye world\n");
+
+  /* Release the lock */
+  serialize_unlock();
 
   return 0;
 }

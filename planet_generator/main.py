1945ef8df297f4b3f01b701c9d5fa0bacb04ada3
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-05-25 00:44:15 +0200

    constant change in planet generator

--- planet_generator/main.py
+++ planet_generator/main.py
@@ -1,5 +1,6 @@
 import random
 
+PLANET_COUNT = 100
 MAX_RADIUS = 10000
 MIN_RADIUS = 1000
 
@@ -16,5 +17,5 @@ class Planet:
         return "{{{{{}, {}}}, {}, false, false}}".format(self.x, self.y, self.radius)
     
 if __name__ == "__main__":
-    for _ in range(30):
+    for _ in range(PLANET_COUNT):
         print(Planet(random.randint(-COORD_LIMIT, COORD_LIMIT), random.randint(-COORD_LIMIT, COORD_LIMIT), random.randint(MIN_RADIUS, MAX_RADIUS)))

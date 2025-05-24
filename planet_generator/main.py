2fdca61ee3f444a19086f0c440c328d9803b382b
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-05-25 00:42:54 +0200

    added planet generator

---
+++ planet_generator/main.py
@@ -0,0 +1,20 @@
+import random
+
+MAX_RADIUS = 10000
+MIN_RADIUS = 1000
+
+COORD_LIMIT = 100000
+
+
+class Planet:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+    
+    def __str__(self):
+        return "{{{{{}, {}}}, {}, false, false}}".format(self.x, self.y, self.radius)
+    
+if __name__ == "__main__":
+    for _ in range(30):
+        print(Planet(random.randint(-COORD_LIMIT, COORD_LIMIT), random.randint(-COORD_LIMIT, COORD_LIMIT), random.randint(MIN_RADIUS, MAX_RADIUS)))

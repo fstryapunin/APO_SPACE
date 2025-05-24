6df254d1a2848f1231f699eedf0b58d1c69cd67d
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-05-25 00:13:25 +0200

    extended vector functions

--- vector.c
+++ vector.c
@@ -1,13 +1,22 @@
 #include "vector.h"
-#include "math.h"
 
-double get_distance(Vector a, Vector b)
-{
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+double get_magnitude_from_vector(Vector vector){
+    return sqrt(pow(vector.x, 2) + pow(vector.y, 2));
+}
+
+double get_magnitude_from_coordinates(double x, double y){
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+double get_distance(Vector a, Vector b){
+    return get_magnitude_from_coordinates(a.x - b.x, a.y - b.y);
 };
 
-Vector rotate(Vector vector, double theta_rad)
-{
+double get_angle(Vector vector){
+    return atan2(vector.y, vector.x);
+}
+
+Vector rotate_vector(Vector vector, double theta_rad){
     double theta_cos = cos(theta_rad);
     double theta_sin = sin(theta_rad);
 
@@ -16,3 +25,23 @@ Vector rotate(Vector vector, double theta_rad)
         vector.x * theta_sin + vector.y * theta_cos
     };
 };
+
+Vector add_vectors(Vector a, Vector b){
+    return (Vector){ a.x + b.x, a.y + b.y };
+}
+
+Vector subtract_vectors(Vector a, Vector b){
+    return (Vector){ a.x - b.x, a.y - b.y };
+}
+
+Vector get_vector_from_angle(double angle){
+    return (Vector){ cos(angle), sin(angle) };
+}
+
+Vector multiply_vector_by_scalar(Vector vector, double scalar){
+    return (Vector){ scalar * vector.x, scalar * vector.y };
+}
+
+Vector get_vector_from_angle_and_magnitude(double angle, double magnitude){
+    return multiply_vector_by_scalar(get_vector_from_angle(angle), magnitude);
+}
--- vector.h
+++ vector.h
@@ -1,8 +1,24 @@
+#ifndef VECTORS
+#define VECTORS
 typedef struct 
 {
     double x;
     double y;
 } Vector;
 
+
+double get_magnitude_from_vector(Vector vector);
+double get_magnitude_from_coordinates(double x, double y);
 double get_distance(Vector a, Vector b);
-Vector rotate(Vector vector, double theta_rad);
+double get_angle(Vector vector);
+
+Vector rotate_vector(Vector vector, double theta_rad);
+Vector add_vectors(Vector a, Vector b);
+Vector subtract_vectors(Vector a, Vector b);
+
+Vector multiply_vector_by_scalar(Vector vector, double scalar);
+
+Vector get_vector_from_angle(double angle);
+Vector get_vector_from_angle_and_magnitude(double angle, double magnitude);
+
+#endif
